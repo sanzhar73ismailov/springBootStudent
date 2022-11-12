c9c0e1b908f8a0b9e01b436f4266b15305741364
sismailov <sanzhar.ismailov@ocado.com>
2022-11-12 22:41:31 +0100

    swagger 3

--- src/main/java/Controller/Controller.java
+++ src/main/java/Controller/Controller.java
@@ -8,13 +8,21 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import Model.Student;
 import Service.Student_Service;
 
+@Tag(name = "student service", description = "the student API with description tag annotation")
 @RestController
 @CrossOrigin(origins="http://localhost:4200")
 @RequestMapping(value="/api")
@@ -22,7 +30,13 @@ public class Controller {
 	
 	@Autowired
 	private Student_Service studentservice;
-	
+
+	@Operation(summary = "Create new student")
+	@ApiResponses({
+			@ApiResponse(responseCode = "200", description = "Student created successfully", content = @Content(schema = @Schema(implementation = Student.class))),
+			@ApiResponse(responseCode = "400", description = "Student parameters not valid"),
+			@ApiResponse(responseCode = "500", description = "Internal Server Error")
+	})
 	@PostMapping("save-student")
 	public boolean saveStudent(@RequestBody Student student) {
 		 return studentservice.saveStudent(student);
